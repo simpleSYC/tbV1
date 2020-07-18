24935262548738f34e520d03f1f5f8d7001c32e1
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-07-18 19:43:53 +0100

    gg
    gg

--- talebook_files/data/K/prK.js
+++ talebook_files/data/K/prK.js
@@ -1,6 +1,10 @@
 K=[
-k_0=[null,"Sorta :","dieta","sport","deserto",0,0,0,"Lehrgeschichten V1","malhbuch (dinos)"," Tezina :","15 kg","33.14 kg","8 kg","73 kg","1 kg",0,13.46],
-k_1=[null,"Sorta :","protein +","focus",0,0,0,0,"Lehrgeschichten V2","malhbuch (exterier)"," dlozia :","2 m","166 cm","7.77 mm",0,0,0,46.48],
-k_2=[null,"Sorta :","ekstra a",0,0,0,0,0,"Lehrgeschichten V3","malhbuch (mosaik)","boja :",0,0,0,0,0,0,46.31]]
+K_0=["Macedonian","Lehrgeschichten V1","A4 malhbuch (dinos)","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,9.64,7,9,3,12,212,3.75,0,0,0,0,0,89.987],
+K_1=["Macedonian","Lehrgeschichten V2","A4 malhbuch (citys)","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,9.64,0,0,0,0,0,3.75,0,0,0,0,0,123.54],
+K_2=["Macedonian","Lehrgeschichten V3","A4 malhbuch (mosaik)","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,9.64,0,0,0,0,0,3.75,0,0,0,0,0,987,789],
+K_3=["Internacional","Hobby malhbuch","V1","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,13.15,0,0,0,0,0,0,0,0,0,0,0,852.111],
+K_4=["Internacional","Hobby malhbuch","V2","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,13.15,0,0,0,0,0,0,0,0,0,0,0,777.666],
+K_5=["Internacional","10 verschtecken  Verschiden <br>& <br> Malhbuch","10v","REALbook A4","book","book & wooden crayons ","book & pencile case( wooden crayons & 2x pencils & gumi)",0,0,0,"e-book.(PDF)","e-book PDF","e-book PDF +Adds 1 page",0,0,0,0,7.37,0,0,0,0,0,0,0,0,0,0,0,9987465]
 
 
+]
